333b7dcccc29e8f0f721c69b11257ff13e9cacc3
shakilkhatri <43198367+shakilkhatri@users.noreply.github.com>
2023-09-24 23:53:57 +0530

    first working commit

---
+++ server.js
@@ -0,0 +1,34 @@
+import { Server } from "socket.io";
+import { createServer } from "http";
+
+let info = ""
+
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+  cors: {
+    origin:"*",
+    methods:["GET","POST"],
+    credentials: true
+  },
+});
+
+const port = 4000;
+httpServer.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+io.on("connection", (socket) => {
+  io.emit('serverMsg', info);
+
+  socket.on("getInfo",()=>{
+    io.emit('serverMsg', info);
+  })
+
+  socket.on("clientUpdate", (text) => {
+    // console.log(text);
+    info = text;
+    io.emit('serverMsg', text);
+  });
+});
